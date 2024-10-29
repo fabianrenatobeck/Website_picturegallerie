dceded76afa84b30b49d339a51f40e181695c012
fabianrenatobeck <153930911+fabianrenatobeck@users.noreply.github.com>
2024-10-29 11:16:41 +0100

    Gallery Fix

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,10 @@
 // src/components/Gallery.jsx
-import React from 'react';
 import '../styles/Gallery.css'; // Importieren des spezifischen CSS-Styles für die Galerie
 
 const Gallery = () => {
     // Beispielbilder - ersetze die URLs durch deine eigenen Bilder
     const images = [
-        { id: 1, src: 'https://recipecontent.fooby.ch/16024_3-2_480-320.jpg', alt: 'Bild 1' },
+        { id: 1, src: 'https://via.placeholder.com/200', alt: 'Bild 1' },
         { id: 2, src: 'https://via.placeholder.com/200', alt: 'Bild 2' },
         { id: 3, src: 'https://via.placeholder.com/200', alt: 'Bild 3' },
         { id: 4, src: 'https://via.placeholder.com/200', alt: 'Bild 4' },
